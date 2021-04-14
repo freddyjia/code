3b6a566a2c692698afc7fa39716d723b99436f43
freddy <freddy@gmail.com>
2021-04-14 22:40:02 +0800

    测试

--- Client/assets/Scripts/Global/Language.ts
+++ Client/assets/Scripts/Global/Language.ts
@@ -6,4 +6,10 @@ export default class Language
     public static text6 = "Network re-connected";
     public static text7 = "Connection failed, please check your network";
     public static text8 = "Network data sending failed";
+    public static CopySuccess = "复制成功";
+    public static Chargetip1 = "金额不能为空";
+    public static Chargetip2 = "请填写正确金额";
+    public static Chargetip3 = "请选择银行";
+    public static Chargetip4 = "图片加载失败";
+
 }
